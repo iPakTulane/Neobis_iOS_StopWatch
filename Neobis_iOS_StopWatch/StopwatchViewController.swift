85ccdcee9cd1dc3a492b3d2537228f9db84e39cb
iPakTulane <ipak.tulane@gmail.com>
2023-10-24 17:51:35 +0500

    Add view controllers of timer and stopwatch alongside respective views

---
+++ Neobis_iOS_StopWatch/StopwatchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StopwatchViewController.swift
+//  Neobis_iOS_StopWatch
+//
+//  Created by Igor Pak on 23/10/23.
+//
+
+import UIKit
+
+class StopwatchViewController: UIViewController {
+    
+    view.backgroundColor = UIColor.yellow
+    
+}
---
+++ Neobis_iOS_StopWatch/TimerViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TimerViewController.swift
+//  Neobis_iOS_StopWatch
+//
+//  Created by Igor Pak on 23/10/23.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.yellow
+ 
+
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//        let timerViewController = TimerViewController.storyboardInstance()
+
+//extension TimerViewController {
+//    static func storyboardInstance() -> TimerViewController? {
+//        let storyboard = UIStoryboard(name: String(describing: self), bundle: nil)
+//        return storyboard.instantiateInitialViewController() as? TimerViewController
+//    }
+//}
+
